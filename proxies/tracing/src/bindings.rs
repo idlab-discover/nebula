d4efb1d1487e6b3b82c688321a76c202222d5333
Ewout <ewoutverlinde@hotmail.com>
2026-04-15 20:19:31 +0200

    feat: first version of tracing proxy

---
+++ proxies/tracing/src/bindings.rs
@@ -0,0 +1,5 @@
+
+wit_bindgen::generate!({
+	world: "tracing",
+	generate_all
+});
--- proxies/tracing/src/lib.rs
+++ proxies/tracing/src/lib.rs
@@ -1,65 +1,111 @@
+mod bindings;
+
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+use bindings::exports::splicer::tier1::after::Guest as AfterGuest;
+use bindings::exports::splicer::tier1::before::Guest as BeforeGuest;
+use bindings::wasi::clocks::wall_clock;
+use bindings::wasi::otel::tracing;
+use bindings::wasi::random::random;
+
 use crate::bindings::export;
-use crate::bindings::exports::nebula::service as service_export;
-use crate::bindings::exports::wasi::http as http_export;
-use crate::bindings::nebula::service as service_import;
-use crate::bindings::wasi::http as http_import;
 
-pub mod bindings;
+const PACKAGE_NAME: &str = "nebula:tracing";
 
-pub struct Proxy;
+struct InflightSpan {
+	context: tracing::SpanContext,
+	start_time: wall_clock::Datetime,
+	parent_span_id: String,
+	name: String,
+}
 
-impl service_export::orders::Guest for Proxy {
-	fn create_order(
-		order: service_export::orders::Order,
-	) -> service_export::orders::Quote {
-		println!("Creating order: {:?}", order);
-		let order = service_import::orders::Order {
-			order_id: "IT WORKS!!".to_string(),
-			..order
+
+
+thread_local! {
+	static INFLIGHT_SPANS: RefCell<HashMap<String, InflightSpan>> = RefCell::new(HashMap::new());
+}
+
+struct TracingProxy;
+
+impl BeforeGuest for TracingProxy {
+	/// Called before every invocation of a target-interface function.
+	#[allow(async_fn_in_trait)]
+	async fn before_call(name: String) {
+		// Create a new span context for the call, using the current outer span context as the parent.
+		let parent_context = tracing::inner_span_context();
+		let span_id = format!("{:016x}", random::get_random_u64());
+
+		let context = tracing::SpanContext {
+			trace_id: parent_context.trace_id.clone(),
+			span_id: span_id.clone(),
+			trace_flags: tracing::TraceFlags::SAMPLED,
+			is_remote: true,
+			trace_state: parent_context.trace_state.clone(),
 		};
-		let result = service_import::orders::create_order(&order);
-		println!("Order created: {:?}", result);
-		result
-	}
 
-	fn delete_order(order_id: String) -> () {
-		println!("Deleting order: {}", order_id);
-		service_import::orders::delete_order(&order_id);
-		println!("Order deleted");
-	}
+		let start_time =  wall_clock::now();
+		let parent_span_id = parent_context.span_id;
 
-	fn read_order(order_id: String) -> Option<service_export::orders::Order> {
-		println!("Reading order: {}", order_id);
-		let result = service_import::orders::read_order(&order_id);
-		println!("Order read: {:?}", result);
-		result
-	}
-}
+		INFLIGHT_SPANS.with(|spans| {
+			let mut spans = spans.borrow_mut();
 
-impl service_export::quotes::Guest for Proxy {
-	fn read_quote(order_id: String) -> Option<service_export::quotes::Quote> {
-		println!("Reading quote for order: {}", order_id);
-		let result = service_import::quotes::read_quote(&order_id);
-		println!("Quote received: {:?}", result);
-		result
-	}
+			let inflight_span = InflightSpan {
+				context: context.clone(),
+				parent_span_id: parent_span_id.clone(),
+				start_time,
+				name: name.clone(),
+			};
 
-	fn delete_quote(order_id: String) -> () {
-		println!("Deleting quote for order: {}", order_id);
-		service_import::quotes::delete_quote(&order_id);
-		println!("Quote deleted");
+			spans.insert(
+				span_id,
+				inflight_span,
+			);
+		});
+
+		tracing::on_start(&context);
 	}
 }
 
-impl http_export::incoming_handler::Guest for Proxy {
-	fn handle(
-		request: http_export::incoming_handler::IncomingRequest,
-		response_out: http_export::incoming_handler::ResponseOutparam,
-	) -> () {
-		println!("Received HTTP request: {:?}", request);
-		http_import::incoming_handler::handle(request, response_out);
-		println!("HTTP request handled");
+impl AfterGuest for TracingProxy {
+	/// Called after every invocation of a target-interface function.
+	#[allow(async_fn_in_trait)]
+	async fn after_call(_name: String) {
+		let current_context = tracing::inner_span_context();
+		let end_time = wall_clock::now();
+
+		let inflight = INFLIGHT_SPANS.with(|spans| spans.borrow_mut().remove(
+			&current_context.span_id));
+
+		let Some(inflight) = inflight else {
+			// This can happen if the guest calls `tracing::on_end` itself, in which case we won't have an inflight span for it.
+			return;
+		};
+
+		let data = tracing::SpanData {
+			span_context: inflight.context,
+			parent_span_id: inflight.parent_span_id,
+			name: inflight.name,
+			start_time: inflight.start_time,
+			span_kind: tracing::SpanKind::Internal,
+			end_time,
+			attributes: vec![],
+			events: vec![],
+			links: vec![],
+			status: tracing::Status::Unset,
+			instrumentation_scope: tracing::InstrumentationScope {
+				name: PACKAGE_NAME.to_string(),
+				version: None,
+				schema_url: None,
+				attributes: vec![],
+			},
+			dropped_attributes: 0,
+			dropped_events: 0,
+			dropped_links: 0,
+		};
+
+		tracing::on_end(&data);
 	}
 }
 
-export!(Proxy with_types_in crate::bindings);
+export!(TracingProxy with_types_in bindings);
